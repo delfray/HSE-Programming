7c5ea0a2eaa8dde28accca819a7571a86d3ec926
delfray <31654770+delfray@users.noreply.github.com>
2017-12-07 11:56:00 +0300

    Delete Kontrolnaya rabota.py

--- kontrolnaya rabota/Kontrolnaya rabota.py
+++
@@ -1,5 +0,0 @@
-with open("freq.txt", encoding="utf-8") as f:
-    for line in f:
-        words = line.split('|')
-        if words[1].find('гл') != -1 and words[1].find('перех') != -1:
-            print(line)
